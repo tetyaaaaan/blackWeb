267016843aa9e8999d637806b092cfe76de85d4f
tetyaaaaan <54570470+tetyaaaaan@users.noreply.github.com>
2021-05-27 23:20:31 +0900

    Update main.js

--- docs/Piano_yaruo/main.js
+++ docs/Piano_yaruo/main.js
@@ -3,6 +3,7 @@ var list = ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4",
             "Cp3", "Dp3", "Fp3", "Gp3", "Ap3" ];
 
 const url = "https://tetyaaaaan.github.io/";
+
 var ext = '.wav';
 if((new Audio()).canPlayType('audio/ogg') == 'maybe') { ext = '.ogg'; }
 for(var i=0; i<list.length; i++){
@@ -24,29 +25,32 @@ for(var i=0; i<list.length; i++){
 
 // キーボード入力に対応させる
 var keydata = [
-               { key : "Z", sound : "C3" },
-               { key : "X", sound : "D3" },
-               { key : "C", sound : "E3" },
-               { key : "V", sound : "F3" },
-               { key : "B", sound : "G3" },
-               { key : "N", sound : "A3" },
-               { key : "M", sound : "B3" },
-               { key : String.fromCharCode(188), sound : "C4" },
-               { key : "S", sound : "Cp3" },
-               { key : "D", sound : "Dp3" },
-               { key : "G", sound : "Fp3" },
-               { key : "H", sound : "Gp3" },
-               { key : "J", sound : "Ap3" }
+               { key : "Z", sound : "C3", isPressing : false },
+               { key : "X", sound : "D3", isPressing : false },
+               { key : "C", sound : "E3", isPressing : false },
+               { key : "V", sound : "F3", isPressing : false },
+               { key : "B", sound : "G3", isPressing : false },
+               { key : "N", sound : "A3", isPressing : false },
+               { key : "M", sound : "B3", isPressing : false },
+               { key : String.fromCharCode(188), sound : "C4", isPressing : false },
+               { key : "S", sound : "Cp3", isPressing : false },
+               { key : "D", sound : "Dp3", isPressing : false },
+               { key : "G", sound : "Fp3", isPressing : false },
+               { key : "H", sound : "Gp3", isPressing : false },
+               { key : "J", sound : "Ap3", isPressing : false }
                ]
 document.addEventListener("keydown", function(e){
                           var k = String.fromCharCode(e.keyCode);
                           for(var i=0; i<keydata.length; i++){
                           if (k == keydata[i].key){
+                          if(!keydata[i].isPressing){
                           var path = "audio/"+keydata[i].sound+ext;
                           var audioObj = new Audio(path);
                           audioObj.volume = 0.3;
                           audioObj.play();
+                          }
                           pressing(keydata[i].sound);
+                          keydata[i].isPressing=true;
                           }
                           }
                           }, true);
@@ -56,6 +60,7 @@ document.addEventListener("keyup", function(e){
                           for(var i=0; i<keydata.length; i++){
                           if (k == keydata[i].key){
                           nopressing(keydata[i].sound);
+                          keydata[i].isPressing=false;
                           }
                           }
                           }, true);
